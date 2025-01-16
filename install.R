c2141d118acd4b156ee31b36b86f553e9030f505
James Gilbert <jgrg@sanger.ac.uk>
2025-01-16 16:51:15 +0000

    Add required dependencies to install.R

--- install.R
+++ install.R
@@ -1,1 +1,10 @@
-install.packages('.', repos=NULL, type="source")
+pkgs <- c(
+  "argparse",
+  "cowplot",
+  "ggplot2",
+  "minpack.lm",
+  "scales",
+  "viridis"
+)
+install.packages(pkgs, dependencies = TRUE)
+install.packages(".", repos = NULL, type = "source")
